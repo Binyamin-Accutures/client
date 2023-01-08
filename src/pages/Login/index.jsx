519a56ae17e9a7ca9d31bfebcf314fea20810118
sapir cafri <sapircafri@gmail.com>
2023-01-08 17:07:00 +0200

    merge whit david

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,45 @@
+import styles from "./style.module.css";
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
+import axios from 'axios'
+
+// creator: david hakak
+// color: _______________
+// icon: ________________
+
+function Login() {
+  const UserEmailInput = useRef(null);
+  const UserPasswordInput = useRef(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  
+      axios.get('https://restcountries.com/v3.1/all')
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+    console.log(UserEmailInput.current.value);
+    // localStorage.setItem("token",JSON.stringify("Bearer "+response.data[1]))
+    localStorage.setItem("token",JSON.stringify("Bearer "+"111111"))
+  }
+
+  return (
+    <form className={styles.formLogin} onSubmit={handleSubmit}>
+      <input type="text" id="username" ref={UserEmailInput} />
+
+      <input type="password" id="password" ref={UserPasswordInput} />
+      <button type="submit">Login</button>
+
+      <div className={styles.formOptions}>
+        <p>forgot password</p>
+        <p>new account</p>
+      </div>
+    </form>
+  );
+}
+
+export default Login;
--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
+import Login from '../pages/Login'
 import Aviad from './Aviad'
+import Sapir from './Sapir'
 
 export const MainTest = () => {
 
     return (
         <Routes>
             <Route path='/aviad' element={<Aviad />} />
+            <Route path='/sapir' element={<Login />} />
+
         </Routes>
     )
 }
