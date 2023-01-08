92d1a1f4b52ac09d3b28548f3f1567e5e9aa1d90
Noa Amichai Kapach <mishkapach@gmail.com>
2023-01-08 15:00:09 +0200

    update global css

--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Aviad from './Aviad'
+import NoaTest from './NoaTest'
 
 export const MainTest = () => {
 
     return (
         <Routes>
             <Route path='/aviad' element={<Aviad />} />
+            <Route path='/noa' element={<NoaTest />} />
         </Routes>
     )
 }
