9833b86535435f99c0878066153a7bfcc771317e
Yair Ken <yair.k.ken@gmail.com>
2023-01-08 14:13:02 +0200

    start_Dynamic

--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -1,10 +1,12 @@
 import Aviad from './Aviad'
+import Yair from './Yair'
 
 export const Test = () => {
 
     return (
         <Routes>
             <Route path='/aviad' element={<Aviad />} />
+            <Route path='/yair' element={<Yair />} />
         </Routes>
     )
 }
---
+++ src/tests/Yair.jsx
@@ -0,0 +1,3 @@
+export default function Yair() {
+    return <> yair</>
+}
