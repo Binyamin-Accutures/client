a4e00f8484322712692efc5153c6f4a82c29a279
elisha izak <elizak20@gmail.com>
2023-01-08 15:44:52 +0200

    section border

--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -1,11 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
+import SectionBorder from '../components/SectionBorder'
 import Aviad from './Aviad'
+import Elisha from './Elisha'
 
 export const MainTest = () => {
 
     return (
         <Routes>
             <Route path='/aviad' element={<Aviad />} />
+            <Route path='/elisha' element={<SectionBorder border='dashed' height='auto' width='auto'>
+                <Elisha/>
+                <Elisha/>
+                <Elisha/>
+                <Elisha/>
+                <Elisha/>
+                <Elisha/>
+
+            </SectionBorder>} />
         </Routes>
     )
 }
