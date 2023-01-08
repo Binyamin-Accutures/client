d716dc35d516db1bc80cadadfa26e76af13a454f
matanelVatkin <matanelVatkin@gmail.com>
2023-01-08 17:03:18 +0200

    add main pages and routing

--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Aviad from './Aviad'
+import Matanel from './Matanel'
 
 export const MainTest = () => {
 
     return (
         <Routes>
             <Route path='/aviad' element={<Aviad />} />
+            <Route path='/matanel/*' element={<Matanel />} />
         </Routes>
     )
 }
