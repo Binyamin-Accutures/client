f563927553c4dbf01bf50cc0d23cb9892de4dceb
ShaharMarom2 <106477834+ShaharMarom2@users.noreply.github.com>
2023-01-08 18:02:51 +0200

    range bar

--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Aviad from './Aviad'
+import Shahar from './Shahar'
 
 export const MainTest = () => {
 
     return (
         <Routes>
             <Route path='/aviad' element={<Aviad />} />
+            <Route path='/shahar' element={<Shahar />} />
         </Routes>
     )
 }
