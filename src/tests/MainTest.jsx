12ffc2fb638b183535705355631d1afb58629db3
Aviad Elya <0527563488a@gmail.com>
2023-01-08 17:45:54 +0200

    creat input and css for him, need valid func from props

--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Aviad from './Aviad'
+import AviadElya from './AviadElya'
 
 export const MainTest = () => {
 
     return (
         <Routes>
             <Route path='/aviad' element={<Aviad />} />
+            <Route path='/aviadElya' element={<AviadElya />} />
         </Routes>
     )
 }
