620bf7c02c50ffdf6f2cb81d24e951833e1afee5
David Hakak <davidhakak@h-MacBook-Pro-sl-David.local>
2023-01-08 15:20:03 +0200

    base login page without css

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,34 @@
+import styles from "./style.module.css";
+import React, { useState } from "react";
+import { useRef } from "react";
+
+// creator: david hakak
+// color: _______________
+// icon: ________________
+
+function Login() {
+  const UserEmailInput = useRef(null);
+  const UserPasswordInput = useRef(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(UserEmailInput.current.value);
+  }
+
+  return (
+    <form className={styles.formLogin} onSubmit={handleSubmit}>
+      <input type="text" id="username" ref={UserEmailInput} />
+
+      <input type="password" id="password" ref={UserPasswordInput} />
+
+      <button type="submit">Login</button>
+
+      <div className={styles.formOptions}>
+        <p>forgot password</p>
+        <p>new account</p>
+      </div>
+    </form>
+  );
+}
+
+export default Login;
--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Aviad from './Aviad'
+import David from './David'
 
 export const MainTest = () => {
 
     return (
         <Routes>
             <Route path='/aviad' element={<Aviad />} />
+            <Route path='/david' element={<David />} />
         </Routes>
     )
 }
