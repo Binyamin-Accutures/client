504136ef699c0d576400b8071718fd4afa8a3a44
Aviad Elya <0527563488a@gmail.com>
2023-01-08 21:00:01 +0200

    after pull

--- src/tests/AviadElya.jsx
+++ src/tests/AviadElya.jsx
@@ -2,17 +2,7 @@ import React from 'react'
 import ChangeFrame from '../components/ChangeFrame'
 
 
-function AviadElya({ type }) {
-
-
-    // const valid = (e) => {
-    //     if (type == 'email') { return setValidi(true) }
-    //     else if (type == 'password' && e.target.value.includes(`{(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6}$`)) {
-    //         return (setValidi(true))
-    //     }
-    //     else return setValidi(false)
-
-    // }
+function AviadElya() {
 
     return (
     <ChangeFrame/>
--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -5,7 +5,7 @@ import Matanel from './Matanel'
 import Elisha from './Elisha'
 import NoaTest from './NoaTest'
 import AviadElya from './AviadElya'
-import David from './David'
+// import David from './David'
 import Shahar from './Shahar'
 import Hadar from './Hadar'
 import Moran from './Moran'
@@ -15,7 +15,7 @@ export const MainTest = () => {
     return (
         <Routes>
             <Route path='/aviad' element={<Aviad />} />
-            <Route path='/david' element={<David />} />
+            {/* <Route path='/david' element={<David />} /> */}
             <Route path='/shahar' element={<Shahar />} />
             <Route path='/hadar' element={<Hadar />} />
             <Route path='/matanel/*' element={<Matanel />} />
