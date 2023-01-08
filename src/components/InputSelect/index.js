8bea228eb970e658f7a292450681ecad90d0d6e6
Hadar Naiman <hadarni@gmail.com>
2023-01-08 17:56:20 +0200

    adding padding

--- src/components/InputSelect/index.js
+++ src/components/InputSelect/index.js
@@ -5,11 +5,11 @@ import styles from "./style.module.css"
 // color: _______________
 // icon: ________________
 
-export default function InputSelect({ label, options, width }) {
+export default function InputSelect({ label, options, width, setSelectInput }) {
     return (<div >
         <label className={`styles.${label}`} forhtml="select">{label}</label><br />
-        <select id={styles.select} style={{ width: `${width}px` }} onChange={(e) => console.log(e.target.value)}>
-            {options.map((v, i, arr) => <option key={i} >{v}</option>)}
+        <select id={styles.select} style={{ width: `${width}px` }} onChange={(e) => setSelectInput(e.target.value)}>
+            {options.map((v, i, arr) => <option key={i} className={styles.option} >{v}</option>)}
         </select>
     </div >
     );
