ad837b5eeb84825bc1a65ff0a3327b5e9e95e8dc
matanelVatkin <matanelVatkin@gmail.com>
2023-01-09 15:48:41 +0200

    chenge user to true

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import './global.css';
 import Layout from './layout';
 import { ContextProvider } from './context/manageContext'
-import { useState } from "react"
-import {MainTest} from './tests/MainTest'
+import { useEffect, useState } from "react"
+import { MainTest } from './tests/MainTest'
 import LayoutLogin from './LayoutLogin';
 
 
 
 function App() {
   const [user, setUser] = useState(false)
+  useEffect(() => {
+    const startApp = async () => {
+      const userFromServer = await apiCalls("get", "https://localhost:5000/api/user")
+      if (userFromServer) {
+        setUser(userFromServer)
+      }
+    }
+
+    if (!user&&localStorage.token) startApp()
+  }, [])
+
+
   return (
 
     <div>
 
-      {(!user) && <><LayoutLogin setUser={setUser}/></>}
-      {user &&<>
-      <ContextProvider user={user} setUser={setUser}>
-        <MainTest/>
-        <Layout/>
-      </ContextProvider>
+      {(!user) && <><LayoutLogin setUser={setUser} /></>}
+      {user && <>
+        <ContextProvider user={user} setUser={setUser}>
+          <MainTest />
+          <Layout />
+        </ContextProvider>
       </>}
 
     </div>
