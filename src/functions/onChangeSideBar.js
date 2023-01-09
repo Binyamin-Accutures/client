65a8949eb7d609adc5f14a2a0a23854948c412eb
pablo lichtik <you@example.com>
2023-01-09 16:22:18 +0200

    trying to make the pull work

--- src/functions/onChangeSideBar.js
+++ src/functions/onChangeSideBar.js
@@ -1,5 +1,5 @@
 
-export default function onChangeSideBar(son, field, value, beforeISPTemp, setBeforeISPTemp) {
+ function onChangeSideBar(son, field, value, beforeISPTemp, setBeforeISPTemp) {
     setBeforeISPTemp(beforeISPTemp[son][field] = value)
 }
 
