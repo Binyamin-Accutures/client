d16c91c3755938bb7027a16747f4d8e40c373c23
Dael Molodic <Dael140@gmail.com>
2023-01-09 09:42:15 +0200

    rgb and hsv component manipulation

--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Aviad from './Aviad'
+import Dael from './Dael'
 
 export const MainTest = () => {
 
     return (
         <Routes>
             <Route path='/aviad' element={<Aviad />} />
+            <Route path='/dael' element={<Dael />} />
         </Routes>
     )
 }
