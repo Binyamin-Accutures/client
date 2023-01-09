2495c7ce995e8f05fd0de62851c8c6c89fb83c5e
Chananel Perlmutter <hprlmwtr@gmail.com>
2023-01-09 16:16:23 +0200

    checkbox1

--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -1,35 +1,15 @@
-
-import Roye from './Roye'
-
 import { Route, Routes } from 'react-router-dom'
-import ChangeFrame from '../components/ChangeFrame'
 import Aviad from './Aviad'
-import Matanel from './Matanel'
-import NoaTest from './NoaTest'
-import AviadElya from './AviadElya'
-import David from './David'
-import Shahar from './Shahar'
-import Hadar from './Hadar'
-import Moran from './Moran'
-import YisraelTest from './YisraelTest'
-
+import Chananel from './Chananel'
 
 export const MainTest = () => {
 
+    
+
     return (
         <Routes>
-            <Route path='/aviad' element={<Aviad />} />
-            <Route path='/elisha' element={<ChangeFrame/>} />
-            <Route path='/roye' element={<Roye/>} />
-            <Route path='/david' element={<David />} />
-            <Route path='/shahar' element={<Shahar />} />
-            <Route path='/hadar' element={<Hadar />} />
-            <Route path='/matanel/*' element={<Matanel />} />
-            <Route path='/noa' element={<NoaTest />} />
-            <Route path='/aviadElya' element={<AviadElya />} />
-            <Route path='/moran' element={<Moran />} />
-            <Route path='/Yisrael' element={<YisraelTest />} />
-
+            <Route path='/Aviad' element={<Aviad />} />
+            <Route path='/Chananel' element={<Chananel />} />
         </Routes>
     )
 }
