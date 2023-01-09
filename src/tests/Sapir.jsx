d6d20c1408a4eb162491a5bc2528e6abd64fc113
yosefus <yosalsoni@gmail.com>
2023-01-09 19:27:06 +0200

    after merge

--- src/tests/Sapir.jsx
+++ src/tests/Sapir.jsx
@@ -1,20 +1,16 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import HistoryImage from "../components/HistoryImage";
+// import HistoryImage from "../components/HistoryImage";
 import InputSelect from "../components/InputSelect";
 import { setToken } from "../functions/apiRequest";
 
 export default function Sapir() {
-
-
-    return(
-    <div>
-        <HistoryImage src={'https://pps.whatsapp.net/v/t61.24694-24/311019981_881128723230304_1836867762896045726_n.jpg?ccb=11-4&oh=01_AdQHa0JTI5h4aQ2YwULUtNHRLhcz3t2_PaKElYywXdLxJg&oe=63C944FC'} time={"ff"} date={"44"} imgName={"pic"}/>
-   
-    </div>
-    
-
-    )}
+    return (
+        <div>
+            {/* <HistoryImage src={'https://pps.whatsapp.net/v/t61.24694-24/311019981_881128723230304_1836867762896045726_n.jpg?ccb=11-4&oh=01_AdQHa0JTI5h4aQ2YwULUtNHRLhcz3t2_PaKElYywXdLxJg&oe=63C944FC'} time={"ff"} date={"44"} imgName={"pic"} /> */}
+        </div>
+    )
+}
 
     // const logOut = () =>{
         //     localStorage.clear();
