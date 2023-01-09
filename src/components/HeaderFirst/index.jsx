198b6d5b75182d3ed9c7f9011e0fb48350a44bb0
adelraz <116503817+adelraz@users.noreply.github.com>
2023-01-09 10:21:26 +0200

    commit 9/1 pirst

--- src/components/HeaderFirst/index.jsx
+++ src/components/HeaderFirst/index.jsx
@@ -11,19 +11,6 @@ function HeaderFirst() {
     <>
     <header className={styles.headerContainer}>
         <div className={styles.logo}><img src={logo} alt='logo'/>  Accutures </div>
-        {/* <div className={styles.navContainer}>
-            <NavLink  className={styles.navitem} to='/loadimage'>
-                    Load Image
-            </NavLink>
-            <NavLink  className={styles.navitem} to='/calibration'>
-                    Calibration
-            </NavLink>
-            <NavLink  className={styles.navitem} to='/history'>History</NavLink>
-        </div>
-        <div className={styles.userBar}>
-        <NavLink  to='/login' className={styles.userName}>
-        <BiUser className={styles.userIcon}/>UserName@mail.co.il</NavLink>
-        </div> */}
     </header>
     </>
   )
--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -2,9 +2,6 @@ import { Route, Routes } from 'react-router-dom'
 import SectionBorder from '../components/SectionBorder'
 import Aviad from './Aviad'
 import Adel from './Adel'
-import Hadar from './Hadar'
-import Matanel from './Matanel'
-import Elisha from './Elisha'
 import NoaTest from './NoaTest'
 import Moran from './Moran'
 
@@ -15,9 +12,9 @@ export const MainTest = () => {
         <Routes>
             <Route path='/aviad' element={<Aviad />} />
             <Route path='/adel' element={<Adel />} />
-            <Route path='/hadar' element={<Hadar />} />
+            {/* <Route path='/hadar' element={<Hadar />} />
             <Route path='/matanel/*' element={<Matanel />} />
-            <Route path='/elisha' element={<Elisha />} />
+            <Route path='/elisha' element={<Elisha />} /> */}
             <Route path='/noa' element={<NoaTest />} />
             <Route path='/moran' element={<Moran />} />
         </Routes>
