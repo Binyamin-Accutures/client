bdda6f0516c0d587ad67bc19f383fc1986277c1c
adelraz <116503817+adelraz@users.noreply.github.com>
2023-01-09 15:57:35 +0200

    4th ver

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import './global.css';
 import Layout from './layout';
 import { ContextProvider } from './context/manageContext'
 import { useState } from "react"
-import {MainTest} from './tests/MainTest'
+import { MainTest } from './tests/MainTest'
 import LayoutLogin from './LayoutLogin';
 
 
 
 function App() {
-  const [user, setUser] = useState(false)
+  const [user, setUser] = useState(true)
   return (
 
     <div>
 
-      {(!user) && <><LayoutLogin setUser={setUser}/></>}
-      {user &&<>
-      <ContextProvider user={user} setUser={setUser}>
-        <MainTest/>
-        <Layout/>
-      </ContextProvider>
+      {(!user) && <><LayoutLogin setUser={setUser} /></>}
+      {user && <>
+        <ContextProvider user={user} setUser={setUser}>
+          <MainTest />
+          <Layout />
+        </ContextProvider>
       </>}
 
     </div>
