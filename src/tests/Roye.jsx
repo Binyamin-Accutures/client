18902fa5e1aa1aeb5294b8e0f0006f4d20370c8f
roye <royeplg@gmail.com>
2023-01-09 15:53:37 +0200

    marge and commit team

--- src/tests/Roye.jsx
+++ src/tests/Roye.jsx
@@ -3,7 +3,8 @@ import CollepseTopDown from '../components/CollepseTopDown'
  import styles from "./style.module.css"
 import ImageContext from '../context/ImageContext'
 
-export default function Roye() {
+export default function Roye(props) {
+
 const menuList = [{titel:"NUC",component:<div><ul><li>2</li><li>2</li></ul></div> },{titel:'Dynamic Range Stretch',component:<div>roey</div> },{titel:'Demosaic',component:<div>roey</div> },{titel:'Denoise',component:<div>roey</div> },{titel:'Sharpening',component:<div>roey</div> }]
 const  value = useContext(ImageContext)
 
