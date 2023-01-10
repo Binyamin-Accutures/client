e00c10906453a39a151a7d5df6aa25d0f2ad3e1d
AviadDerli <aviad@workin.co.il>
2023-01-10 15:10:03 +0200

    fix header view

--- src/pages/HistoryPage/index.jsx
+++ src/pages/HistoryPage/index.jsx
@@ -3,9 +3,7 @@ import styles from "./style.module.css"
 import Header from '../../components/Header'
 export default function HistoryPage() {
   return (
-    <header className={styles.header}>
-      <Header />
-    </header>
+    <></>
 
   )
 }
