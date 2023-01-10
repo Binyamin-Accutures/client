046daa9df22a4cd0310ea2fb6d1b5f9b6302bd95
Yair Ken <yair.k.ken@gmail.com>
2023-01-10 19:55:44 +0200

    DRS_context2

--- src/components/DynamicRangeStretch/index.jsx
+++ src/components/DynamicRangeStretch/index.jsx
@@ -3,104 +3,47 @@ import ImageContext from "../../context/ImageContext"
 import Checkbox from "../Checkbox"
 import InputSelect from "../InputSelect"
 import RangeSlider from "../RangeSlider"
-import styles from "./style.module.css"
 
 // creator: yair ken
-// data : יש בו את כל המידע ועידכון המידע
-// classNameTitle="titl" וכו'
-// 
-
-const data1 = { 
-   enable : true ,
-   method : {
-       histogram : '',
-       BDP : 0,
-       DDP : 0
-   }
-}
-
-const data2 = { 
-   enable : true,
-   method : {
-       enable : true,
-       method : "dynamic_range_stretch",
-       cutoffs: {
-           method: "percent",
-           bitdepth: 16,
-           low: 0,
-           high: 100,
-           high_value: 100
-         }
-   }
-}
-
-const data3= { 
-   enable : true,
-   method : {
-       method : "dynamic_range_stretch",
-       cutoffs: {
-           method: "percent",
-           bitdepth: 16,
-           low: 0,
-           high: 100,
-           high_value: 100
-         }
-   }
-}
 
 const DynamicRangeStretch = ({data ,style = {},className="", classNameTitle = "" ,classNameOption="", classNameDropTitle="" , ...props }) => {
    
    const value = useContext(ImageContext)
 
-   const handle = (e)=>{
-      let name = e.name
-      let valueToChange = e.checked
-      value.setBeforeISP(preve=>{
-      const newobj =  {...preve}
-         newobj.DRS[name]=valueToChange
+   const handleHightLevel = (e) => {
+      let name = e.name;
+      let valueToChange;
+      e.type == "checkbox" ? valueToChange = e.checked : valueToChange = e.value;
+      value.setBeforeISP(preve => {
+         const newobj = { ...preve }
+         newobj.sharpening[name] = valueToChange;
          return newobj
       })
-   }
+   }    
 
-      const handleForSelectdle = (e)=>{
-         let name = e.name
-         let valueToChange = e.value
-         value.setBeforeISP(preve=>{
-         const newobj =  {...preve}
-            newobj.DRS.method[name]=valueToChange
-            return newobj
-         })
-      
-          }
-// let defultSelect = value.beforeISP.DRS
-// let clone = {}
-// //   let's copy all user properties into it
-//    for (let key in defultSelect) {
-//    clone[key] = defultSelect[key];
-//    console.log(defultSelect);
-//    }
-// `${defultSelect.method.method}`,
-
-
-
+   const handle = (e)=>{
+      let name = e.name
+      let valueToChange = e.value
+      value.setBeforeISP(preve=>{
+      const newobj =  {...preve}
+         newobj.DRS.method.cutoffs[name]=valueToChange
+         return newobj
+      })
+   
+       }
 
    return (
       <div className={`styles.${classNameTitle} ${className}`} style={style} {...props} id={""} >
          <div className={`styles.${classNameOption}`}>
-            <Checkbox label="enable" name="enable" onChange={handle} checked={value.beforeISP.DRS.enable}/>
+            <Checkbox label="enable" name="enable" onChange={handleHightLevel} checked={value.beforeISP.DRS.enable}/>
          </div>
          <div className={`styles.${classNameDropTitle} ${classNameOption}`}  >
-            <InputSelect label="method" options={[`${value.beforeISP.DRS.method.method}`,"Historgram"]} width="265" selected={value.beforeISP.DRS.method.method} handle={handleForSelectdle}/>
-            <RangeSlider func={handle} text="Bright Discards Percentile" min={0} max={100} step={1} textPosLeft={false} numInput={false}/>
+            <InputSelect label="method" options={["bla","percent"]} width="265"handle={handle} dataDefault={value.beforeISP.DRS.method.cutoffs.method}/>
+            <RangeSlider func={handle} text="Bright Discards Percentile" name="low" min={0} max={100} step={1} textPosLeft={false} numInput={false} contextValue={value.beforeISP.DRS.method.cutoffs.low}/>
          </div>
          <div className={`styles.${classNameOption}`}>
-            <RangeSlider func={()=>console.log("onChange-range")} text="Dark Discard Percentile" min={0} max={100} step={1} textPosLeft={false} numInput={false}/>
+            <RangeSlider func={handle} text="Dark Discard Percentile" name="high" min={0} max={100} step={1} textPosLeft={false} numInput={false} contextValue={value.beforeISP.DRS.method.cutoffs.high}/>
          </div>
-      
-         <div>{`checkbox: ${value.beforeISP.DRS.enable}`}</div>
-         <div>{`select: ${value.beforeISP.DRS.method.method}`}</div>
-         <div>{`range: ${value.beforeISP.DRS.method.cutoffs.bitdepth}`}</div>
-
       </div>
       
    )
