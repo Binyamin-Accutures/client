44ca4f3a0e37453caf9aa2c180b1e32d3dacc811
sapir cafri <sapircafri@gmail.com>
2023-01-10 15:51:24 +0200

    log out function

--- src/components/Logout/index.jsx
+++ src/components/Logout/index.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom';
 import styles from "./style.module.css";
 import { BiLogOutCircle } from 'react-icons/bi'
+import UserContext from '../../context/UserContext';
 
 // creator:moran and adel
 // data : logout button, apeare when the user icon is pressed
-function logOut({ setIsShow }) {
+
+function LogOut({ setIsShow }) {
+    const {setIsLogged, setUser } = useContext(UserContext)
+
+    const logOutClicked = () => {
+        localStorage.clear();
+        setUser('');
+        setIsLogged(false);
+    }
     return (
         <>
             <div className={styles.logoutComp} >
                 <BiLogOutCircle className={styles.logoutIcon} /><br />
 
-                <button className={styles.logoutBtn}>logOut</button>
+                <button onClick={logOutClicked} className={styles.logoutBtn}>logOut</button>
+                
             </div>
             <div className={styles.closeBtn} onClick={() => setIsShow(false)}></div>
         </>
@@ -19,4 +29,4 @@ function logOut({ setIsShow }) {
     )
 }
 
-export default logOut
+export default LogOut
