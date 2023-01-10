dbd5e6c3bfbec5fbe2fb578856f967688ac077a7
Yisrael Olonoff <yisrael94@gmail.com>
2023-01-10 18:17:11 +0200

    finished component image preview

--- src/components/ImagePreview/index.jsx
+++ src/components/ImagePreview/index.jsx
@@ -1,22 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import styles from './style.module.css'
 import InputLoadImage from '../InputLoadImage'
 import Header from '../Header'
+import ChangeFrame from '../ChangeFrame'
+import RangeSlider from '../RangeSlider'
+import Button from '../Button'
+import Carousel from '../Carousel'
+import ImageContext from "../../context/ImageContext";
+
+
+// Creator: Yisrael Olonoff
+// 
 
 
 
 function ImagePreview() {
 
-    const [load, setLoad] = useState(false)
+    const [load, setLoad] = useState(false);//[true, empty]
+    const value = useContext(ImageContext);
+    const [selectedImage, setSelectedImage] = useState(0) // 1 is the default
+    console.log(value)
+
+   const handleChange = (target) => {
+      setSelectedImage(target.value)
+   }
 
     return (
         <>
             <Header />
-            <div className={styles.imgprevContainner}>
-            <div className={styles.ImagePreview}>
-                <InputLoadImage  width={'133px'} > Load Image </InputLoadImage>
-                <p>or drag and drop image here </p>
-            </div>
+            <div className={styles.imgprevContainner}>     
+                   {!load && <div className={styles.loadImagePage} > < InputLoadImage width={'133px'} setLoad={setLoad} >
+                        Load Image </InputLoadImage><p>or drag and drop image here </p></div>}                
+                {load && 
+                <form >
+                    <div className={styles.ImagePreview}>
+                        <img className={styles.images} src={new Object(value.beforeISP.images[selectedImage]).url}/>
+                        <div className={styles.controls}>
+                        <RangeSlider className={styles.RangeSlider} func={handleChange} min={0} max={value.beforeISP.images.length-1}/>
+                        <ChangeFrame className={styles.ChangeFrame} func={handleChange} min={selectedImage.value} max={value}/>
+                        <Button width={328} type='submit' >Next</Button>
+                        </div>
+                    </div>
+                </form>}
             </div>
         </>
     )
