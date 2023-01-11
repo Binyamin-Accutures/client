7ae64594a9737b9f6f9530ef97643d19340924cc
adelraz <116503817+adelraz@users.noreply.github.com>
2023-01-11 17:22:24 +0200

    11/1 17:20

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useNavigate, NavLink } from 'react-router-dom';
 import logo from './logo.svg';
 import styles from './style.module.css'
 import { BiUser } from 'react-icons/bi'
 import Logout from '../Logout';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+
 
 
 
@@ -16,6 +17,13 @@ import { useLocation } from "react-router-dom";
 
 
 function Header() {
+
+
+    const navigate = useNavigate()
+    const [isNext, setIsNext] = useState(true)
+    const [isBack, setIsBack] = useState(false)
+
+
     const [isShow, setIsShow] = useState(false)
     //assigning location variable
     const location = useLocation();
@@ -25,6 +33,18 @@ function Header() {
 
     //Javascript split method to get the name of the path in array
     const splitLocation = pathname.split("/");
+
+    const next = () => {
+        navigate('/calibration')
+        setIsNext(false)
+        setIsBack(true)
+    }
+
+    const back = () => {
+        navigate('/loadimage')
+        setIsNext(true)
+        setIsBack(false)
+    }
 
     return (
         <>
@@ -41,10 +61,10 @@ function Header() {
                     <NavLink className={splitLocation[1] === "history" ? styles.active : styles.navitem} to='/history'>History</NavLink>
                 </div>
                 <div className={styles.space} />
-                <div className={styles.backNext}>
-                    <div className={styles.backNextOff}>  <IoIosArrowBack className={styles.backNextOff} />   Back</div>
-                    <div className={styles.backNextOff}>Next    <IoIosArrowForward className={styles.backNextOff} /></div>
-                </div>
+                {splitLocation[1] !== "history" && <div className={styles.backNext}>
+                    <div className={isBack ? styles.backNextOn : styles.backNextOff} onClick={back}>  <IoIosArrowBack className={isBack ? styles.backNextOn : styles.backNextOff} />   Back</div>
+                    <div className={isNext ? styles.backNextOn : styles.backNextOff} onClick={next}>Next    <IoIosArrowForward className={isNext ? styles.backNextOn : styles.backNextOff} /></div>
+                </div>}
                 <div className={styles.userBar}>
 
                     <BiUser className={styles.userIcon} onClick={() => setIsShow(!isShow)} >
