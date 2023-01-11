7147ad40be66c8381016710f1e9ce20d1a07e891
nadav <nadavbzk@gmail.com>
2023-01-11 18:49:24 +0200

    shorter version

--- src/functions/convertCanvasToFormData.js
+++ src/functions/convertCanvasToFormData.js
@@ -1,5 +1,5 @@
-//This module-function gets an array of canvases 
-// and returns them in FormData 
+//This module-function gets an object of arrays of canvases 
+// and returns them all in FormData 
 
 // //Test-Example Arrrrrrrrrrrrrrrrrrrrrrrrrrrrrr 
 // var canvasArr = [];
@@ -10,31 +10,46 @@
 //     canvasArr.push(canvas);
 // }
 
-export function convertCanvasToFormData(canvasArr = []) {
+export function convertCanvasToFormData(canvasObj) {
     let counter = 1;
     const formData = new FormData();
-    for(canvas of canvasArr){
-        let index = counter < 10 ? "_0" + counter : "_" + counter;
-        counter++;
-        var dataURL = canvas.toDataURL();
-        var blob = dataURItoBlob(dataURL);
-        formData.append("image" + index, blob, "image" + index + ".png");
-        console.log(formData.get("image" + index));
+    Object.entries(canvasObj).forEach((field) => {
+        for(canvas of field){
+            console.log("canvas: ", canvas);
+            let index = counter < 10 ? "_0" + counter : "_" + counter++;
+            console.log("counter: ", counter);
+            let dataURL = canvas.toDataURL();
+            formData.append(field + "", dataURL, field + index + ".png");
     }
+    })
     return formData;
 }
 
-function dataURItoBlob(dataURI) {
-    var byteString = atob(dataURI.split(',')[1]);
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-    var ab = new ArrayBuffer(byteString.length);
-    var ia = new Uint8Array(ab);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-    var blob = new Blob([ab], { type: mimeString });
-    return blob;
-}
+// export function convertCanvasToFormData0(canvasArr = [],categoryName = "") {
+//     let counter = 1;
+//     const formData = new FormData();
+//     for(canvas of canvasArr){
+//         let index = counter < 10 ? "_0" + counter : "_" + counter;
+//         counter++;
+//         var dataURL = canvas.toDataURL();
+//         var blob = dataURItoBlob(dataURL);
+//         formData.append(categoryName + index, blob, categoryName + index + ".png");
+//         console.log(formData.get(categoryName + index));
+//     }
+//     return formData;
+// }
+
+// function dataURItoBlob(dataURI) {
+//     var byteString = atob(dataURI.split(',')[1]);
+//     var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+//     var ab = new ArrayBuffer(byteString.length);
+//     var ia = new Uint8Array(ab);
+//     for (var i = 0; i < byteString.length; i++) {
+//         ia[i] = byteString.charCodeAt(i);
+//     }
+//     var blob = new Blob([ab], { type: mimeString });
+//     return blob;
+// }
 
 // //check
 // const fd = convertCanvasToFormData(canvasArr);
