303915ec3500480f100730dfded91c6154f229a6
yosefus <yosalsoni@gmail.com>
2023-01-11 15:20:09 +0200

    merge

--- src/tests/MainTest.jsx
+++ src/tests/MainTest.jsx
@@ -1,44 +1,13 @@
 
-
-
-
-
-
 import ChangeFrame from '../components/ChangeFrame';
-import HistoryPage from '../pages/HistoryPage';
 import Itamar from "./Itamar";
-
-
-
-
-
-
-
-
-
-// import LoginPage from '../pages/LoginPage'
-// import Sapir from './Sapir'
 import { Route, Routes } from 'react-router-dom'
-
-
-
-
-
-
 import SectionBorder from '../components/SectionBorder'
 import LoginPage from '../pages/LoginPage'
 import Pavel from './Pavel'
-
 import Roye from "./Roye";
-
-import SectionBorder from "../components/SectionBorder";
-import LoginPage from "../pages/LoginPage";
-import Pavel from "./Pavel";
-
 import Anat from "./Anat";
-
 import Adel from "./Adel";
-
 import Moran from "./Moran";
 import Sapir from "./Sapir";
 import Aviad from "./Aviad";
@@ -48,17 +17,14 @@ import Elisha from "./Elisha";
 import David from "./David";
 import Dael from "./Dael";
 import Shahar from "./Shahar";
-// import Hadar from "./Hadar";
 import AviadElya from "./AviadElya";
 import Noam from "./Noam";
 import Dudu from "./Dudu";
 import YisraelTest from "./YisraelTest";
-
 import HistoryPage from "../pages/HistoryPage";
 import Yair from './Yair';
 import Gil from './Gil';
 import Chananel from './Chananel';
-import Dael from './Dael';
 import Nadav from './Nadav';
 
 export const MainTest = () => {
@@ -73,13 +39,9 @@ export const MainTest = () => {
       <Route path="/roye" element={<Roye />} />
       <Route path="/david" element={<David />} />
       <Route path="/shahar" element={<Shahar />} />
-      {/* <Route path="/hadar" element={<Hadar />} /> */}
       <Route path="/noa" element={<NoaTest />} />
       <Route path="/aviadElya" element={<AviadElya />} />
       <Route path="/Itamar" element={<Itamar />} />
-
-      {/* <Route path='/adel' element={<Adel />} /> */}
-
       <Route path="/adel" element={<Adel />} />
       <Route path="/pavel" element={<Pavel />} />
       <Route path="/sapir" element={<Sapir />} />
@@ -87,8 +49,6 @@ export const MainTest = () => {
       <Route path="/Yisrael" element={<YisraelTest />} />
       <Route path="/dudu" element={<Dudu />} />
       <Route path="/yair" element={<Yair />} />
-
-
       <Route path='/gil' element={<Gil />} />
       <Route path='/Chananel' element={<Chananel />} />
     </Routes>
