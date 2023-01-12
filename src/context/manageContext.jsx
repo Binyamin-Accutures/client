51c1ca87fbc14763d055942f99bba3f712820d76
study <study@Fw-3021-Noamy.fw.local>
2023-01-12 09:57:28 +0200

    ##

--- src/context/manageContext.jsx
+++ src/context/manageContext.jsx
@@ -86,11 +86,11 @@ export const ContextProvider = ({ user,setUser, children }) => {
     
     const [afterISP, setAfterISP] = useState(afterISPTemp);
     const [beforeISP, setBeforeISP] = useState(beforeISPTemp);
-
+    const [currentImages , setCurrentImages]= useState()
 
     return (
         <UserContext.Provider value= {{user, setUser, isLogged, setIsLogged}}>
-            <ImageContext.Provider value={{afterISP, setAfterISP,beforeISP, setBeforeISP}}>
+            <ImageContext.Provider value={{afterISP, setAfterISP,beforeISP, setBeforeISP,currentImages , setCurrentImages}}>
                 {children}
             </ImageContext.Provider>
         </UserContext.Provider>
--- src/tests/Noam.jsx
+++ src/tests/Noam.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import Button from "../components/Button";
 import Image from "../components/Image";
 import InputLoadImage from "../components/InputLoadImage";
@@ -10,23 +10,44 @@ function Noam() {
   const value = useContext(ImageContext);
   let index;
 
+  useEffect(() => {
+    value.setCurrentImages(value.beforeISP.images);
+  }, [value.beforeISP]);
+  console.log(value.currentImages);
+
+  const prepareFilesToServer = (diff) => {
+    let formData = new FormData();
+    for (let i of diff) {
+      formData.append("file", i);
+    }
+    return formData;
+  };
+
+  const uplaodToServer = async () => {
+    console.log(value.currentImages);
+    const srcList = await apiCalls(
+      "post",
+      "https://accutures-server.onrender.com/api/files",
+      prepareFilesToServer(value.currentImages)
+    );
+    console.log(srcList.data);
+
+    let fr = new FileReader();
+    fr.readAsDataURL(
+      `https://accutures-server.onrender.com/upload/jdoe2sddh@5gddmad434dddil.com/1673459626098/original/0.png`
+    );
+    let result = fr.result;
+    console.log(result)
+  };
+
   return (
     <>
       <div>
         <InputLoadImage width={"328px"} />
         <Image value={value} index={index || 0} />
-        <Button func={uplaodToServer()}>Next</Button>
+        <Button func={uplaodToServer}>Next</Button>
       </div>
     </>
   );
 }
-
 export default Noam;
-
-
-// // first update server
-const uplaodToServer = (start, end, condition, files) => {
-
-  // 
-  apiCalls("post", "api/files", files)
-};
