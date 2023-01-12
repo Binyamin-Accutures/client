445496077c2da990b398e703dde5d32ed694f881
Hadar Naiman <hadarni@gmail.com>
2023-01-12 15:47:06 +0200

    pdf file

--- src/components/Pdf/index.jsx
+++ src/components/Pdf/index.jsx
@@ -1,17 +1,10 @@
-// import Nuc from "../Nuc";
-// import DynamicRangeStretch from "../DynamicRangeStretch";
 import { useState, useContext } from "react";
 import ImageContext from "../../context/ImageContext";
-// import Demosaic from "../Demosaic";
-// import Denoise from "../Denoise";
-// import Sharping from "../Sharping";
 import styles from "./style.module.css";
-// import RangeSlider from "../RangeSlider";
-// import { FiSettings } from "react-icons/fi"
-// import ReactPDF, { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
+import { FiSettings } from "react-icons/fi"
+import { GrDocumentPdf } from "react-icons/gr"
+import logo from './logo.svg';
 import JsPDF from 'jspdf';
-
-// ReactPDF.render(<Pdf />, `./example.pdf`)
 
 
 const generatePDF = () => {
@@ -22,107 +15,69 @@ const generatePDF = () => {
     })
 };
 
-
-// import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
-
 export default function Pdf() {
     const value = useContext(ImageContext);
     return (
         <div className={styles.pdf} id="report">
-            <h1 className={styles.header}>IMAGE NAME</h1>
-            <div className={styles.container}>
-                <div className={styles.left_bar}> <label className={styles.label}>NUC</label><br />
-                    {` Enable: ${value.beforeISP.NUC.enable}`}<br />
-                    Method: <br />
-                    {` 1pt: ${value.beforeISP.NUC.method.pt1}`}<br />
-                    {` 2pt: ${value.beforeISP.NUC.method.pt2}`}<br />
-                    {` Bad Pixel Correction: ${value.beforeISP.NUC.method.bad_pixel_correction}`}<br />
+            <div className={styles.logo}><img src={logo} alt='logo' />
+                < div className={styles.container}>
+                    <div className={styles.left_bar}> <label className={styles.label}><b>NUC</b></label><br />
+                        {` Enable: ${value.beforeISP.NUC.enable}`}<br />
+                        Method: <br />
+                        {` 1pt: ${value.beforeISP.NUC.method.pt1}`}<br />
+                        {` 2pt: ${value.beforeISP.NUC.method.pt2}`}<br />
+                        {` Bad Pixel Correction: ${value.beforeISP.NUC.method.bad_pixel_correction}`}<br />
+
+                    </div>
+                    <div className={styles.left_bar}> <label className={styles.label}><b>Dynamic Range Stretch</b></label><br />
+                        {` Enable: ${value.beforeISP.DRS.enable}`}<br />
+                        {`Method: ${value.beforeISP.DRS.method.cutoffs.method}`}<br />
+                        {`Bright Discard Precentile: ${value.beforeISP.DRS.method.cutoffs.low}`}<br />
+                        {` Dark Discard  Precentile: ${value.beforeISP.DRS.method.cutoffs.high}`}<br />
+
+                    </div>
+
+                    <div className={styles.left_bar}> <label className={styles.label}><b>Demosaic</b></label><br />
+
+                        {`Method: ${value.beforeISP.demosaic.method}`}<br />
+                        {`Pixel Order: ${value.beforeISP.demosaic.pixelOrder}`}<br />
+
+
+                    </div>
+
+                    <div className={styles.left_bar}> <label className={styles.label}><b>Denoise</b></label><br />
+                        {`Method: ${value.beforeISP.denoise.method}`}<br />
+                        {`Radius: ${value.beforeISP.denoise.radius}`}<br />
+                        {` ESP: ${value.beforeISP.denoise.ESP}`}<br />
+
+                    </div>
+                    <div className={styles.left_bar}> <label className={styles.label}><b>Sharpening</b></label><br />
+                        {` Enable: ${value.beforeISP.sharpening.enable}`} <br />
+                        {`Method: ${value.beforeISP.sharpening.method}`}<br />
+                        {`Radius: ${value.beforeISP.sharpening.radius}`}<br />
+                        {` ESP: ${value.beforeISP.sharpening.ESP}`}<br />
+                        {` trehold: ${value.beforeISP.sharpening.trehold}`}<br /><br />
+
+                    </div>
+
+
+                    <h3> <FiSettings /> DISPLAY SETTING</h3>
 
                 </div>
-                <div className={styles.left_bar}> <label className={styles.label}>Dynamic Range Stretch</label><br />
-                    {` Enable: ${value.beforeISP.NUC.enable}`}<br />
-                    Method: <br />
-                    {` 1pt: ${value.beforeISP.NUC.method.pt1}`}<br />
-                    {` 2pt: ${value.beforeISP.NUC.method.pt2}`}<br />
-                    {` Bad Pixel Correction: ${value.beforeISP.NUC.method.bad_pixel_correction}`}<br />
+                <div>
+                    <div className={styles.left_bar}> <label className={styles.label}><b>RGB</b></label><br />
+                        {`AoLP Center: ${value.afterISP.displaySet.RGB.AoLPCenter}`}<br />
+                        {`AoLP FOV: ${value.afterISP.displaySet.RGB.AoLPPov}`}<br />
+                        {`Minimum DoLP value: ${value.afterISP.displaySet.RGB.minDoLPVal}`}<br />
+                        {`Maximum DoLP value: ${value.afterISP.displaySet.RGB.maxDoLPVal}`}<br />
+                        {`Minimum S0 value: ${value.afterISP.displaySet.RGB.minS0Value}`}<br />
+                        {`Maximum S0 value: ${value.afterISP.displaySet.RGB.maxS0Value}`}
+                    </div>
 
-                </div>
+                    <button onClick={generatePDF} type="button"><GrDocumentPdf />Export PDF</button>
+                </div >
 
-            </div>
-            <button onClick={generatePDF} type="button">Export PDF</button>
-        </div>
+            </div >
+        </div >
     )
-
-
-
-
-
-
-    // // Create styles
-    // const styles = StyleSheet.create({
-    //     page: {
-    //         flexDirection: 'row',
-    //         backgroundColor: '#E4E4E4'
-    //     },
-    //     section: {
-    //         margin: 10,
-    //         padding: 10,
-    //         flexGrow: 1
-    //     }
-    // });
-
-    // Create Document Component
-    // return (
-    //     <>
-    //         <Document >
-    //             <Page size="A4" style={styles.page}>
-    //                 <View style={styles.section}>
-    //                     <Text>Section #1</Text>
-    //                 </View>
-    //                 <View style={styles.section}>
-    //                     <Text>Section #2</Text>
-    //                 </View>
-    //             </Page>
-    //         </Document>
-    //     </>
-    // );
-
-    // <PDFDownloadLink document={<MyDoc />} fileName="somename.pdf">
-    //     {({ blob, url, loading, error }) =>
-    //         loading ? 'Loading document...' : 'Download now!'
-    //     }
-    // </PDFDownloadLink>
-
-    // ReactPDF.render(<MyDocument />, `${__dirname}/example.pdf`);
-
-
-    // return (
-    //     <div className={styles.pdf}>
-    //         <h1 className={styles.header}>IMAGE NAME</h1>
-    //         <div className={styles.container}>
-    //             <div className={styles.setting}>
-    //                 <div className={styles.left_bar}> <label className={styles.label}>NUC</label><br />
-    //                     <Nuc design={styles.D} /> </div>
-    //                 <div className={styles.left_bar}>  <label className={styles.label}>Demosaic</label><br />
-    //                     <Demosaic design={styles.D} /></div>
-    //                 <div className={styles.sharpening}> <label className={styles.label}>Sharpening</label><br />
-    //                     <Sharping design={styles.D} /></div>
-    //                 <div className={styles.left_bar}>  <label className={styles.label}>Dynamic Range Stretch</label><br />
-    //                     <DynamicRangeStretch design={styles.D} /></div>
-    //                 <div className={styles.left_bar}> <label className={styles.label}>Denoise</label><br />
-    //                     <Denoise design={styles.D} /></div>
-    //             </div>
-    //             <img src="" className={styles.image} />
-    //             <div> <FiSettings /> DISPLAY SETTING</div>
-    //             <div>    {
-
-    //             }</div>
-    //         </div>
-
-
-
-    //     </div >
-
-    // )
-
 }
