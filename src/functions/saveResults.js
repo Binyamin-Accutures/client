d28c78ef613ff32fd001c0e30ae78682a14ff583
Dael Molodic <Dael140@gmail.com>
2023-01-12 10:14:47 +0200

    fix save result

--- src/functions/saveResults.js
+++ src/functions/saveResults.js
@@ -1,7 +1,25 @@
-// import  ImageConversion  from "./ImageConversion";
+import saveImageResult from "./saveImageResult";
+
 const saveResults = (obj) => {
-    console.log(obj);
-    // ImageConversion(obj.imagePath, obj);
+    useEffect(() => {
+        (async () => {
+          const { formData, src } = await saveImageResult(imagesAll, afterISPTemp);
+          setSrc(src);
+    
+          axios.post('http://localhost:5000/upload', formData, {
+              headers: {
+                  'Content-Type': 'multipart/form-data',
+                  'Content-Disposition': 'attachment; filename=example.png'
+              },
+              responseType: 'stream'
+          })
+          .then(response => {
+            console.log(response.data);
+          });
+          console.dir(formData);
+        })()
+      }, []) // todo clean code
+        
 
 }
 export default saveResults;
