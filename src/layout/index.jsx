156c3631094b6ec7eac42afbb9d678f268cd82a8
kobikru <75983501+kobikru@users.noreply.github.com>
2023-02-15 22:51:42 +0200

    after good zip

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom';
-import Header from '../components/Header';
-import CalibrationPage from '../pages/CalibrationPage';
-import HistoryPage from '../pages/HistoryPage';
-import LoadImagePage from '../pages/LoadImagePage';
-import styles from "./style.module.css"
-
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "../components/Header";
+import CalibrationPage from "../pages/CalibrationPage";
+import HistoryPage from "../pages/HistoryPage";
+import LoadImagePage from "../pages/LoadImagePage";
+import styles from "./style.module.css";
 
 export default function Layout() {
   return (
     <div className={styles.layout}>
       <header className={styles.header}>
-            <Header/>
+        <Header />
       </header>
       <div>
-        <Routes >
-            <Route path="/*" element={<LoadImagePage/>}/>
-            <Route path="/calibration" element={<CalibrationPage/>}/>
-            <Route path="/history" element={<HistoryPage/>}/>
+        <Routes>
+          <Route path="/*" element={<LoadImagePage />} />
+          <Route path="/calibration" element={<CalibrationPage />} />
+          <Route path="/history" element={<HistoryPage />} />
         </Routes>
       </div>
     </div>
-  )
+  );
 }
